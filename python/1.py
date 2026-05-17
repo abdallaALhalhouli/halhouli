83f3da0a4aa912f78b85d51f3c18a8789438783f
abdalla ALhalhouli <halhouliabcdefg@gmail.com>
2026-05-17 20:42:42 +0300

    update

---
+++ python/1.py
@@ -0,0 +1,10 @@
+print("hallo this is abdalla and this is my first python code")
+x=" hallo this is a variable"
+print("my first variable is " +x+".")
+name="abdallah"
+print("my name is "+ name + ".")
+
+name= "ahmad" # we can change the value of the variable
+print("my name is "+ name + ".")
+
+# we can make a note by typing # before the note
